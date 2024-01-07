0c071e1ab8ea1e6f1692b7c18d92ea0a3584cbc7
taiseiue <taisei@wsoft.ws>
2024-01-07 16:57:55 +0900

    Alice3.0アップグレード

--- tryalice.wsoft.ws/Program.cs
+++ tryalice.wsoft.ws/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 try
 {
     AliceScript.Runtime.InitBasicAPI();
+    AliceScript.NameSpaces.Alice_Math.Init();
+    AliceScript.NameSpaces.Alice_Reflection.Init();
+    AliceScript.NameSpaces.Alice_Random.Init();
+    AliceScript.NameSpaces.Alice_Regex.Init();
+    AliceScript.NameSpaces.Alice_Security.Init();
+    AliceScript.NameSpaces.Alice_Diagnostics.Init();
+    AliceScript.NameSpaces.Alice_Environment.Init();
+
 }
 catch { }
 
